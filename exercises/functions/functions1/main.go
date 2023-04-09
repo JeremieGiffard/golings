44c316f7bb5894b7c8586fcb4aaf71ef2c9d0ae7
jeremie giffard <e132784f@gmail.com>
2023-04-09 15:55:09 +0200

    function1 to 4 completed

--- exercises/functions/functions1/main.go
+++ exercises/functions/functions1/main.go
@@ -1,9 +1,14 @@
 // functions1
 // Make me compile!
 
-// I AM NOT DONE
 package main
+
+import "fmt"
 
 func main() {
 	call_me()
+}
+
+func call_me() {
+	fmt.Printf("Hey, I just met you, and this is crazy,but here's my number,so call me, maybe?")
 }
